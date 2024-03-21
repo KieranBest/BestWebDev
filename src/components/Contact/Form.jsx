9cd2a92f7b50f7c38427e5d3d03d49db54e47b61
Kieran Best <best.kieran.4595@gmail.com>
2024-03-21 23:21:06 +0000

    Fixed form finally

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -6,30 +6,61 @@ import swal from "sweetalert2";
 export const Form = () => {
     const form = useRef();
 
-    const sendEmail = (e) => {
+    const checkValidation = (e) => {
         e.preventDefault();
+        if (form.current.name.value === "") {
+            swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Name is required!",
+            });
+            return false;
+        } else if (form.current.email.value === "") {
+            swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Email is required!",
+            });
+            return false;
+        } else if (form.current.message.value === "") {
+            swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Message is required!",
+            });
+            return false;
+        } else {
+            swal.fire({
+                icon: "success",
+                title: "Message Sent!",
+                text: "Message is required!",
+            });
+            sendEmail();
+            return true;
+        }
+    }
+
+    const sendEmail = () => {
 
         emailjs
         .sendForm(
             import.meta.env.VITE_EMAIL_SERVICE_ID, 
             import.meta.env.VITE_EMAIL_TEMPLATE_ID, 
             form.current, 
-            {
-                publicKey: import.meta.env.VITE_EMAIL_USER_KEY,
-            }
+            import.meta.env.VITE_EMAIL_USER_KEY,
         )
         .then(
             () => {
-            console.log('SUCCESS!');
+            console.log('Message Sent');
             },
             (error) => {
-            console.log('FAILED...', error.text);
+            console.log('Sending Failed', error.text);
             },
         );
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail} className="font-poppins">
+        <form ref={form} onSubmit={checkValidation} className="font-poppins">
         <label>Name</label>
         <input type="text" name="name" />
         <label>Email</label>
