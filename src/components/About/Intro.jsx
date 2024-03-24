65647c1fbfd0fb439d21f471067c1fcd9b9abdd6
Kieran Best <best.kieran.4595@gmail.com>
2024-03-24 19:54:54 +0000

    Feedback + Build

--- src/components/About/Intro.jsx
+++ src/components/About/Intro.jsx
@@ -22,13 +22,12 @@ export const Intro = () => {
         })
     });
     return (
-        
         <section className="font-poppins" id="vanta">
             <div className="flex h-[90vh] flex-col flex-1 items-center justify-center">
-                <h1 className="mx-5 h-[40vh] text-6xl text-white animate-slidein opacity-0 [--slidein-delay:600ms]">About.</h1>
+                <h1 className="mx-5 h-[40vh] text-6xl text-white animate-slidein opacity-0 [--slidein-delay:600ms]">About</h1>
             </div>
             <div className="flex h-[30vh] flex-col flex-1 items-center justify-center text-center my-1">
-                <p className="m-5 text-md sm:text-2xl text-white">Working as a 1 man team to create a website that you can be proud of.</p>
+                <p className="m-5 text-md sm:text-2xl text-white">Working as a small team to enable bespoke customer service and a website that you can be proud of.</p>
                 <p className="m-5 text-md sm:text-2xl text-white">Charging a minimal amount to ensure that you're business can thrive.</p>
             </div>
         </section>
--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -25,13 +25,11 @@ export const Intro = () => {
     return (
         <section className="font-poppins" id="vanta">
             <div className="flex h-[90vh] items-center justify-center">
-                <h1 className="mx-5 h-[40vh] text-6xl text-white text-center animate-slidein opacity-0 [--slidein-delay:600ms]">Get a Website with <b>No Fuss</b>.</h1>
+                <h1 className="mx-5 h-[40vh] text-6xl text-white text-center animate-slidein opacity-0 [--slidein-delay:600ms]">Get a Website with <b>No Fuss</b></h1>
             </div>
             <div className="flex h-[30vh] items-center justify-center">
                 <p className="mx-5 text-md sm:text-2xl text-white text-center leading-6 sm:leading-10">
-                    Creating websites that are fast, secure, and easy to
-                    manage. We specialize in websites for small businesses,
-                    non-profits, and individuals.
+                    Creating websites that are fast, secure, and easy to manage. We specialize in websites for small businesses, non-profits, and individuals.
                 </p>
             </div>
         </section>
